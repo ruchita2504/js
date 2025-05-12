cb50b84f2b36e0eed9161475bcdb0eb0a8822301
Ruchita Pawar <77008158+ruchita2504@users.noreply.github.com>
2025-05-12 20:17:25 +0530

    Add files via upload

---
+++ app/meals/[mealsSlug]/page.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import classes from './page.module.css';
+import dummyMeals from "@/data";
+import { getMeal } from '@/lib/meals';
+
+export async function generateMetadata({ params }) {
+    const meal = getMeal(params.mealsSlug);
+    if (!meal) {
+        notFound();
+    }
+
+    return {
+        title:meal.title,
+        description:meal.summary,
+
+    };
+}
+
+export default function MealsDetailsPage({params}) {
+    const mealsData = getMeal(params.mealsSlug);
+    if (!mealsData) {
+        notFound();
+    }
+    mealsData.instructions = mealsData.instructions.replace(/\n/g,'<br>')
+    return (
+        <>
+        <header className={classes.header}>
+        <div className={classes.image}>
+                <Image src={mealsData.image} alt={mealsData.title} width="500" height="300"/>
+            </div>
+        <div className={classes.headerText}>
+                <h1>{mealsData.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${mealsData.creator_email}`}>{mealsData.creator}</a>
+                </p>
+                <p className={classes.summary}>{mealsData.summary}</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{__html: mealsData.instructions }}></p>
+        </main>
+        </>
+    );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,64 @@
+'use client';
+import { useActionState } from 'react';
+import classes from './page.module.css';
+import ImagePicker from '@/components/meals/image-picker';
+import { submitMeals } from '@/lib/actions';
+import MealsFormSubmit from "@/components/meals/meals-submit-form";
+
+export default function ShareMealPage() {
+  const [state,formAction] = useActionState(submitMeals,{message:null});
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+      <form className={classes.form} action={formAction}>
+    {/* <form className={classes.form} onSubmit={handleSubmit}> */}
+      <div className={classes.row}>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            required />
+        </p>
+        <p>
+          <label htmlFor="email">Your email</label>
+          <input type="email" id="email" name="email" 
+          required />
+        </p>
+      </div>
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title"  
+        required />
+      </p>
+      <p>
+        <label htmlFor="summary">Short Summary</label>
+        <input type="text" id="summary" name="summary"  
+        required />
+      </p>
+      <p>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          rows="10"
+          required
+        ></textarea>
+      </p>
+      <ImagePicker label ="your image" name="image" />
+      {state.message && <p>{state.message}</p>}
+      <p className={classes.actions}>
+        <MealsFormSubmit />
+      </p>
+    </form>
+      </main>
+    </>
+  );
+}
